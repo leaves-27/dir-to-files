6de8fffe4d3426adb658c2dc7e6162a4fc363985
dong.keke <dong.keke@tongbanjie.com>
2017-01-06 11:56:22 +0800

    添加获取文件列表

---
+++ example.js
@@ -0,0 +1,4 @@
+var file = require("./fe");
+var dir = "/Users/kk/tests/generator-customer/app/templates";
+
+console.log("list1:",file.geFileList(dir));
---
+++ files.js
@@ -0,0 +1,44 @@
+var fs = require('fs')
+
+//遍历文件夹，获取所有文件夹里面的文件信息
+/*
+ * @param path 路径
+ *
+ */
+
+function geFileList(path) {
+  var filesList = [];
+  readFile(path, filesList);
+  return filesList;
+}
+
+//遍历读取文件
+function readFile(path, filesList) {
+  files = fs.readdirSync(path); //需要用到同步读取
+  files.forEach(walk);
+  function walk(file) {
+    states = fs.statSync(path + '/' + file);
+    if (states.isDirectory()) {
+      readFile(path + '/' + file, filesList);
+    } else {
+      //创建一个对象保存信息
+      var obj = new Object();
+      obj.size = states.size; //文件大小，以字节为单位
+      obj.name = file; //文件名
+      obj.path = path + '/' + file; //文件绝对路径
+      filesList.push(obj);
+    }
+  }
+}
+
+//写入文件utf-8格式
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data, 'utf-8',
+  function() {
+    console.log("文件生成成功");
+  });
+}
+
+module.exports = {
+  geFileList: geFileList
+}
